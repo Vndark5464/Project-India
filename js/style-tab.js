7df43695b8a8c1912967f7d6562c9ddba5f7b27c
Vndark5464 <nguyenhuulinh5464@gmail.com>
2022-10-05 22:27:34 +0700

    project

--- js/style-tab.js
+++ js/style-tab.js
@@ -1,3 +1,11 @@
+var loader = function() {
+    setTimeout(function() {
+        $('#loader').css({ 'opacity': 0, 'visibility':'hidden' });
+    }, 1000);
+};
+$(function(){
+    loader();
+});
 /*Hàm Mở Form*/
 function moForm() {
     document.getElementById("myForm").style.display = "block";
